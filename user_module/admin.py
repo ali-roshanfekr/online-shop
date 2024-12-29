2bcc4efddda663dcbaf859033dbe77f68a11f896
Ali Roshanfekr <roshanfekrali5@gmail.com>
2024-12-29 18:08:00 +0330

    set the settings of jalali-date

--- user_module/admin.py
+++ user_module/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import *
-
+from jalali_date.admin import ModelAdminJalaliMixin
 
 # Register your models here.
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'last_login']
+class UserAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['username', 'get_date_fa']
     list_filter = ['is_staff', 'is_active']
 
 
--- user_module/models.py
+++ user_module/models.py
@@ -1,5 +1,7 @@
+import jdatetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import jalali_date
 
 # Create your models here.
 
@@ -7,9 +9,14 @@ class User(AbstractUser):
     phone = models.CharField(max_length=20)
     active_code = models.CharField(max_length=10)
     token = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.username
+
+    def get_date_fa(self):
+        date_join_fa = jdatetime.GregorianToJalali(self.date_joined.year, self.date_joined.month, self.date_joined.day)
+        final_date_join = date_join_fa.getJalaliList()
+        return f'{final_date_join[0]}/{final_date_join[1]}/{final_date_join[2]}'
 
     class Meta:
         verbose_name = 'کاربر'
